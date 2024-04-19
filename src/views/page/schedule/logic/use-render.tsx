2aa38789902e15d6f880728c0c2d9c6af7190b40
obayashi-go <forest.go428@outlook.jp>
2024-04-19 18:51:53 +0900

    refactor schedule

--- src/views/page/schedule/logic/use-render.tsx
+++ src/views/page/schedule/logic/use-render.tsx
@@ -20,18 +20,87 @@ import NightlightIcon from '@mui/icons-material/Nightlight';
 import RedeemIcon from '@mui/icons-material/Redeem';
 import HomeWorkIcon from '@mui/icons-material/HomeWork';
 import LuggageIcon from '@mui/icons-material/Luggage';
-import { Link, Paper, Typography, styled } from "@mui/material"
+import { Box, Link, Modal, Paper, Typography, styled } from "@mui/material"
 import { MenuColorCode } from '../../../../utils/constants/menu-color-code';
+import { useState } from 'react';
 
 const SPaper = styled(Paper)({
     padding: '6px',
 });
 
+const STimelineOppositeContent = styled(TimelineOppositeContent)({
+    display: "flex",
+    flexDirection: "column",
+    gap: "4px"
+});
+
+const MoveTime = styled(Typography)({
+    fontSize: "1rem",
+    background: MenuColorCode.schedule,
+    color: "#fff",
+    padding: "2px 4px",
+    borderRadius: "4px",
+    width: "fit-content"
+})
+
+const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "90%",
+    bgcolor: 'background.paper',
+    border: '1px solid #000',
+    boxShadow: 24,
+    padding: "8px",
+    backgroundImage: 'url(https://d34gglw95p9zsk.cloudfront.net/item_cocreco_images/images/000/023/167/large/fed50829-c8be-44e6-b96d-6fe812ebff74.jpg?1643194360)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  };
+
 export const useRender = () => {
 
     const TimeTable = (): JSX.Element => {
+        const [openSkyBusModal, setOpenSkyBusModal] = useState(false);
+        const handleOpenSkyBusModal = () => {
+            setOpenSkyBusModal(true);
+        }
+        const handleCloseSkyBusModal = () => {
+            setOpenSkyBusModal(false);
+        }
         return (
             <>
+                {openSkyBusModal && (
+                    <Modal
+                    open={openSkyBusModal}
+                    onClose={handleCloseSkyBusModal}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={modalStyle}>
+                      <Typography id="modal-modal-title" fontSize="midium" textAlign="center" sx={{ textShadow: "2px 2px 6px #fff" }}>
+                        スカイバス<br/>
+                        東京タワー・レインボーブリッジコース
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{
+                        margin: "10px auto",
+                        textAlign: "center",
+                        backgroundColor: '#e4e4e421',
+                        borderRadius: "10px",
+                        width: "fit-content",
+                        padding: "2px 4px"
+                        }}>
+                        コース
+                      </Typography>
+                      <Box
+                        component="img"
+                        src='https://www.skybus.jp/course/up_img/1499778376-503372_3.png'
+                        width="100%"
+                      />
+                    </Box>
+                  </Modal>
+                )}
                 <Typography sx={{
                     borderLeft: "solid 4px #4050b5",
                     paddingLeft: "6px",
@@ -318,10 +387,15 @@ export const useRender = () => {
                                 <TimelineConnector />
                             </TimelineSeparator>
                             <TimelineContent>
-                            <SPaper elevation={3}>
-                                <Typography variant="h6" component="h1"  textAlign="center">
-                                スカイバス
-                                </Typography>
+                            <SPaper elevation={3} onClick={handleOpenSkyBusModal}>
+                                <Box display="flex" flexDirection="column">
+                                    <Typography fontWeight={700} textAlign="center">
+                                        スカイバス
+                                    </Typography>
+                                    <Typography fontWeight={700} textAlign="center">
+                                        ヒロおじさんFamily合流！
+                                    </Typography>
+                                </Box>
                                 <Typography textAlign="left">東京タワー・レインボーブリッジコース<br/>
                                 <span style={{
                                     color: "#fff",
@@ -402,23 +476,13 @@ export const useRender = () => {
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineOppositeContent>
-                            {/* <Typography variant="body2" color="textSecondary">
-                                13:30-14:00~
-                            </Typography> */}
                             </TimelineOppositeContent>
                             <TimelineSeparator>
                                 <TimelineDot color="primary" variant='filled' >
                                     <HomeWorkIcon />
                                 </TimelineDot>
-                                {/* <TimelineConnector /> */}
                             </TimelineSeparator>
                             <TimelineContent>
-                            {/* <SPaper elevation={3}>
-                                <Typography variant="h6" component="h1"  textAlign="center">
-                                帰宅
-                                </Typography>
-                                <Typography textAlign="left">お家に帰るまでが遠足</Typography>
-                            </SPaper> */}
                         </TimelineContent>
                     </TimelineItem>
                 </Timeline>
@@ -426,7 +490,179 @@ export const useRender = () => {
         )
     }
 
+    const MoveTimeLine = () => {
+        return (
+            <>
+                <Typography sx={{
+                        borderLeft: "solid 4px #4050b5",
+                        paddingLeft: "6px",
+                        marginLeft: "6px",
+                        lineHeight: 1,
+                        fontSize: "24px"
+                    }}>
+                        5月4日
+                </Typography>
+                <Timeline position="left" sx={{
+                    background: "linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,253,221,1) 20%, rgba(249,255,128,1) 40%, rgba(255,253,221,1) 60%, rgba(202,163,255,1) 80%, rgba(255,255,255,1) 100%)"
+                    }}>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                        08:00
+                        <MoveTime>10分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='primary' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>レンタカー</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>8:30-9:00</Typography>
+                            <MoveTime>30分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='secondary' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>ママさん・ババさんご乗車</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>9:00-9:30</Typography>
+                            <MoveTime>1時間〜<br/>1時間30分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='secondary' variant='outlined' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>大林家乗車</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>10:00-11:00</Typography>
+                            <MoveTime>10分〜20分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='warning' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>駐車場到着</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>11:00-11:30</Typography>
+                            <MoveTime>11分〜20分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='success' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>ホテル荷物預け</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>11:30-12:00</Typography>
+                            <MoveTime>5分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='info' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>雷門到着</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>17:30</Typography>
+                            <MoveTime>6分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='success' variant='outlined' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>天ぷら屋さん</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>18:30-19:00</Typography>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='error' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>ホテル</TimelineContent>
+                    </TimelineItem>
+                </Timeline>
+                <Typography sx={{
+                    borderLeft: "solid 4px #4050b5",
+                    paddingLeft: "6px",
+                    marginLeft: "6px",
+                    lineHeight: 1,
+                    fontSize: "24px"
+                }}>
+                    5月5日
+                </Typography>
+                <Timeline position="left" sx={{background: "linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(249,255,128,1) 40%, rgba(241,144,93,1) 80%, rgba(255,255,255,1) 100%)"}}>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                        09:30-09:30
+                        <MoveTime>10分〜20分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='primary' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>ホテルチェックアウト</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>10:00</Typography>
+                            <MoveTime>10分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='secondary' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>駐車場</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>10:15前後</Typography>
+                            <MoveTime>10分前後</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='secondary' variant='outlined' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>スカイバス発着</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>11:00-11:30</Typography>
+                            <MoveTime>1時間〜<br/>1時間30分</MoveTime>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='warning' />
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>東京駅</TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <STimelineOppositeContent color="text.secondary">
+                            <Typography>16:00-17:00</Typography>
+                        </STimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot color='success' />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{fontSize: "small"}}>おうち</TimelineContent>
+                    </TimelineItem>
+                </Timeline>
+            </>
+        )
+    }
+
     return {
-        TimeTable
+        TimeTable,
+        MoveTimeLine
     }
 }
--- src/views/page/schedule/schedule.tsx
+++ src/views/page/schedule/schedule.tsx
@@ -1,15 +1,32 @@
-import { type FC } from "react"
+import { useState, type FC } from "react"
 import { PageTitle } from "../../conponents/PageTitle"
 import { ContentsInnerWrap } from "../../conponents/contents-inner-wrap"
 import { useRender } from "./logic/use-render";
+import { FormControlLabel, FormGroup, Switch } from "@mui/material";
 
 export const Schedules: FC = () => {
-    const { TimeTable } = useRender();
+    const { TimeTable, MoveTimeLine } = useRender();
+    const [isMoveTime, setIsMoveTime] = useState(false);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsMoveTime(event.target.checked);
+    };
     return (
         <>
             <PageTitle title="スケジュール" pageType="schedule" />
             <ContentsInnerWrap>
-                <TimeTable />
+                <FormGroup sx={{alignContent: "flex-end"}}>
+                    <FormControlLabel
+                        control={
+                        <Switch
+                            checked={isMoveTime}
+                            onChange={handleChange}
+                        />
+                        }
+                        label="移動時間"
+                    />
+                </FormGroup>
+                {!isMoveTime && (<TimeTable />)}
+                {isMoveTime && (<MoveTimeLine />)}
             </ContentsInnerWrap>
         </>
     )
